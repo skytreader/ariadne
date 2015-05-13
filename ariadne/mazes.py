806b68229b1b2b9a49be54bcfed566754f7fb26a
Chad Estioco <chadestioco@gmail.com>
2015-05-14 02:33:25 +0800

    Add to string functionality.

--- ariadne/mazes.py
+++ ariadne/mazes.py
@@ -113,3 +113,36 @@ class Maze(object):
 
         if will_open_west:
             self.__set_cell_state(row, col - 1, MazeCellStates.OPEN_EAST)
+
+    def __str__(self):
+        build_string = ["".join([" _" for _ in range(len(self.maze) - 1)].push(" "))]
+        
+        for row in self.maze:
+            row_str = []
+            for cell in row:
+                open_east = (cell & MazeCellStates.OPEN_EAST) == MazeCellStates.OPEN_EAST
+                open_south = (cell & MazeCellStates.OPEN_SOUTH) == MazeCellStates.OPEN_SOUTH
+                open_west = (cell & MazeCellStates.OPEN_WEST) == MazeCellStates.OPEN_WEST
+                
+                cell_str = []
+
+                if open_east:
+                    cell_str.push(" ")
+                else:
+                    cell_str.push("|")
+
+                if open_south:
+                    cell_str.push(" ")
+                else:
+                    cell_str.push("_")
+
+                if open_west:
+                    cell_str.push(" ")
+                else:
+                    cell_str.push("|")
+
+                row_str.push("".join(cell_str))
+
+            build_string.push("".join(row_str))
+        
+        return "\\n".join(build_string)
--- ariadne/tests/mazes_test.py
+++ ariadne/tests/mazes_test.py
@@ -6,28 +6,43 @@ import unittest
 class MazeTest(unittest.TestCase):
     
     def setUp(self):
-        self.maze = Maze(4, 4)
+        self.test_maze4 = Maze(4, 4)
+        self.test_maze3 = Maze(3, 3)
 
     def test_tear_down_wall(self):
-        self.maze.tear_down_wall(2, 2, MazeCellStates.OPEN_WEST)
-        self.assertEqual(self.maze.maze[2][2], MazeCellStates.OPEN_WEST)
+        self.test_maze4.tear_down_wall(2, 2, MazeCellStates.OPEN_WEST)
+        self.assertEqual(self.test_maze4.maze[2][2], MazeCellStates.OPEN_WEST)
 
-        self.maze.tear_down_wall(2, 2, MazeCellStates.OPEN_SOUTH)
-        self.assertEqual(self.maze.maze[3][2], MazeCellStates.OPEN_NORTH)
-        self.assertEqual(self.maze.maze[2][2], MazeCellStates.OPEN_SOUTH_WEST)
+        self.test_maze4.tear_down_wall(2, 2, MazeCellStates.OPEN_SOUTH)
+        self.assertEqual(self.test_maze4.maze[3][2], MazeCellStates.OPEN_NORTH)
+        self.assertEqual(self.test_maze4.maze[2][2], MazeCellStates.OPEN_SOUTH_WEST)
         
-        self.maze.tear_down_wall(2, 2, MazeCellStates.NO_OPEN)
-        self.assertEqual(self.maze.maze[2][2], MazeCellStates.OPEN_SOUTH_WEST)
+        self.test_maze4.tear_down_wall(2, 2, MazeCellStates.NO_OPEN)
+        self.assertEqual(self.test_maze4.maze[2][2], MazeCellStates.OPEN_SOUTH_WEST)
 
     def test_tear_down_wall_validations(self):
-        self.assertRaises(CantTearWallException, self.maze.tear_down_wall, 0, 1,
+        self.assertRaises(CantTearWallException, self.test_maze4.tear_down_wall, 0, 1,
           MazeCellStates.OPEN_NORTH)
-        self.assertRaises(CantTearWallException, self.maze.tear_down_wall, 1, 3,
+        self.assertRaises(CantTearWallException, self.test_maze4.tear_down_wall, 1, 3,
           MazeCellStates.OPEN_EAST)
-        self.assertRaises(CantTearWallException, self.maze.tear_down_wall, 3, 1,
+        self.assertRaises(CantTearWallException, self.test_maze4.tear_down_wall, 3, 1,
           MazeCellStates.OPEN_SOUTH)
-        self.assertRaises(CantTearWallException, self.maze.tear_down_wall, 1, 0,
+        self.assertRaises(CantTearWallException, self.test_maze4.tear_down_wall, 1, 0,
           MazeCellStates.OPEN_WEST)
+
+    def test_str(self):
+        target = """
+ _ _ _ 
+| |_  |
+| | | |
+|_ _ _|"""
+
+        target = target.strip()
+        self.test_maze3.tear_down_wall(0, 0, MazeCellStates.OPEN_SOUTH)
+        self.test_maze3.tear_down_wall(1, 0, MazeCellStates.OPEN_SOUTH)
+        self.test_maze3.tear_down_wall(0, 1, MazeCellStates.OPEN_EAST)
+        self.test_maze3.tear_down_wall(1, 1, MazeCellStates.OPEN_SOUTH)
+        self.test_maze3.tear_down_wall(1, 2, MazeCellStates.OPEN_SOUTH)
 
 
 if __name__ == "__main__":
