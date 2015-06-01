4f621efb46404e1caa97e2b4ed79a5257367637f
Chad Estioco <chadestioco@gmail.com>
2015-06-02 04:32:18 +0800

    Let's be pedantic while we are it.

--- ariadne/errors.py
+++ ariadne/errors.py
@@ -9,3 +9,12 @@ class CantTearWallException(Exception):
         from .mazes import MazeCellState
         return "Can't %s for cell at (%s, %s)" % (MazeCellState.LABELS[self.cell_state],
           self.row, self.col)
+
+class InvalidSizeException(Exception):
+    
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return "Invalid size: width=%s, height=%s" % (self.width, self.height)
--- ariadne/tests/mazes_test.py
+++ ariadne/tests/mazes_test.py
@@ -1,4 +1,4 @@
-from ..errors import CantTearWallException
+from ..errors import CantTearWallException, InvalidSizeException
 from ..mazes import Maze, MazeCellStates
 
 import unittest
@@ -10,6 +10,11 @@ class MazeTest(unittest.TestCase):
         self.test_maze3 = Maze(3, 3)
         self.rect_maze = Maze(4, 3)
         self.small_rect = Maze(3, 2)
+
+    def test_invalid_setup(self):
+        self.assertRaises(InvalidSizeException, Maze, -2, 1)
+        self.assertRaises(InvalidSizeException, Maze, 2, -1)
+        self.assertRaises(InvalidSizeExcpetion, Maze, -2, -1)
 
     def test_tear_down_wall(self):
         self.test_maze4.tear_down_wall(2, 2, MazeCellStates.OPEN_WEST)
