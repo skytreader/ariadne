db60fb645678a43532011ba212f822115a41e5b0
Chad Estioco <chadestioco@gmail.com>
2015-06-02 20:45:11 +0800

    More shallow tests.

--- ariadne/tests/generators_test.py
+++ ariadne/tests/generators_test.py
@@ -7,7 +7,8 @@ class RecursiveBacktrackerTest(unittest.TestCase):
     """
     Some shallow tests for the generator.
 
-    Just makes sure that all cells have been carved.
+    Just makes sure that all cells have been carved and that the generated maze
+    is up to spec..
     """
 
     def setUp(self):
@@ -19,3 +20,12 @@ class RecursiveBacktrackerTest(unittest.TestCase):
         for row in gen_maze.maze:
             for cell in row:
                 self.assertNotEqual(cell, MazeCellStates.NO_OPEN)
+
+        gen_maze = self.generator.generate(2, 3)
+
+        for row in gen_maze.maze:
+            self.assertEqual(len(row), 2)
+            for cell in row:
+                self.assertNotEqual(cell, MazeCellStates.NO_OPEN)
+
+        self.assertEqual(len(gen_maze.maze), 3)
