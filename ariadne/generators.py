29945fee0e0ecc340740fc9e07229b5ae53f07b5
Chad Estioco <chadestioco@gmail.com>
2015-06-04 12:35:11 +0800

    Seems-to-work Eller's Algorithm.
    Try playing with the probability of tearing down EAST walls. The mazes are not
    winding enough due to not enough EAST walls torn down.

--- ariadne/generators.py
+++ ariadne/generators.py
@@ -84,11 +84,12 @@ class EllersAlgorithm(MazeGenerator):
         def make_sets_and_merge(maze, cur_row_index, set_count = None):
             if set_count is not None:
                 # This is the first case set_count must not be None
-                # The first merge is always uniformly-distributed.
                 start = 0
                 for limit in range(set_count, width, set_count):
                     for cur_cell in range(start, set_count):
-                        maze.tear_down_wall(cur_row_index, cur_cell, MazeCellStates.OPEN_EAST)
+                        if random.choice(list(range(10))) < 7:
+                            print("Tear EAST %s, %s" % (cur_row_index, cur_cell))
+                            maze.tear_down_wall(cur_row_index, cur_cell, MazeCellStates.OPEN_EAST)
                     start += set_count + 1
             else:
                 """
@@ -100,23 +101,43 @@ class EllersAlgorithm(MazeGenerator):
                 set_bounds = []
                 last_cell_state = None
 
-                for ci in range(width):
-                    if last_cell_state and 
-                      (maze.maze[cur_row_index][ci] & MazeCellStates.OPEN_NORTH) != (last_cell_state & MazeCellStates.OPEN_NORTH):
+                for ci in range(1, width):
+                    if (maze.maze[cur_row_index][ci] & MazeCellStates.OPEN_NORTH) != (maze.maze[cur_row_index][ci - 1] & MazeCellStates.OPEN_NORTH):
                         set_bounds.append(ci - 1)
 
-                    last_cell_state = maze.maze[cur_row_index][ci]
-
+                print("Set boundaries %s" % set_bounds)
                 # Do the merge
                 for bound in set_bounds:
-                    if random.choice((True, False)):
+                    print("Try tear EAST %s, %s" % (cur_row_index, bound))
+                    if random.choice(list(range(10))) < 7:
                         maze.tear_down_wall(cur_row_index, bound, MazeCellStates.OPEN_EAST)
                         
 
         maze = Maze(width, height)
         # Ensure sets won't be singletons
-        set_count = random.choice(range(width - 1))
+        set_count = random.choice(range(1, width - 1))
         divisions = math.floor(width / set_count)
         # Construct the initial sets. They are in a tuple to indicate adjacency
+
+        for i in range(height):
+            if i == 0:
+                make_sets_and_merge(maze, i, set_count)
+            else:
+                make_sets_and_merge(maze, i)
+
+            # With the sets made, merge vertically, at least once per set.
+            is_curset_merged = False
+            if i != (height - 1):
+                for j in range(width):
+                    is_new_set_next = (maze.maze[i][j] & MazeCellStates.OPEN_EAST) != MazeCellStates.OPEN_EAST
+
+                    if is_new_set_next and not is_curset_merged:
+                        maze.tear_down_wall(i, j, MazeCellStates.OPEN_SOUTH)
+                    elif random.choice((True, False)):
+                        maze.tear_down_wall(i, j, MazeCellStates.OPEN_SOUTH)
+                        is_curset_merged = True
+
+                    if is_new_set_next:
+                        is_curset_merged = False
         
         return maze
